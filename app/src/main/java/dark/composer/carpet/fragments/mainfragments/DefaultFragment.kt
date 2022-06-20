5c876335009c5914dcb1e9a52ef0de7b1845dad1
mustafo898 <mustafoabdugafurov55@gmail.com>
2022-06-20 21:45:51 +0500

    s

--- app/src/main/java/dark/composer/carpet/fragments/mainfragments/DefaultFragment.kt
+++ app/src/main/java/dark/composer/carpet/fragments/mainfragments/DefaultFragment.kt
@@ -1,14 +1,64 @@
 package dark.composer.carpet.fragments.mainfragments
 
+import androidx.recyclerview.widget.LinearLayoutManager
 import dark.composer.carpet.R
+import dark.composer.carpet.adapter.CategoryAdapter
+import dark.composer.carpet.adapter.PopularAdapter
 import dark.composer.carpet.databinding.FragmentDefault1Binding
 import dark.composer.carpet.databinding.FragmentDefaultBinding
+import dark.composer.carpet.dto.CategoryModel
 import dark.composer.carpet.fragments.BaseFragment
 
-class DefaultFragment : BaseFragment<FragmentDefault1Binding>(FragmentDefault1Binding::inflate) {
-    override fun onViewCreate() {
-//        binding.addBtn.setOnClickListener {
-//            navController.navigate(R.id.action_defaultFragment_to_logInFragment)
-//        }
+class DefaultFragment : BaseFragment<FragmentDefaultBinding>(FragmentDefaultBinding::inflate) {
+    private val adapterCategory by lazy {
+        CategoryAdapter(requireContext())
     }
+
+    private val adapterPopular by lazy {
+        PopularAdapter(requireContext())
+    }
+
+    override fun onViewCreate() {
+        binding.listCategory.layoutManager = LinearLayoutManager(requireContext(),
+            LinearLayoutManager.HORIZONTAL,false)
+        binding.listPopular.layoutManager = LinearLayoutManager(requireContext(),
+            LinearLayoutManager.HORIZONTAL,false)
+        binding.listCategory.adapter = adapterCategory
+        binding.listPopular.adapter = adapterPopular
+
+        adapterCategory.set(setCategory())
+        adapterPopular.set(setPopular())
+
+        binding.txtCustomers.isSelected = true
+        binding.txtAdmin.isSelected = true
+        binding.txtEmployee.isSelected = true
+        binding.floatingActionButton.setOnClickListener {
+
+        }
+    }
+
+    private fun setCategory():List<CategoryModel>{
+        val list = mutableListOf<CategoryModel>()
+        for (i in 0 until 10){
+            list.add(CategoryModel("Shu zor chiqibti ggggggggggg",R.drawable.carpet,"1203369 cym"))
+        }
+        return list
+    }
+
+    private fun setPopular():List<CategoryModel>{
+        val list = mutableListOf<CategoryModel>()
+        for (i in 0 until 10){
+            list.add(CategoryModel("Shu zor chiqibti",R.drawable.carpet,"1203369 cym"))
+        }
+        return list
+    }
+//    private fun startTimer() {
+//        Handler().postDelayed({
+//            if (shared.getToken() != "") {
+//                extention.controller?.startMainFragment(TrainerFragment())
+//            } else {
+//                extention.controller?.startMainFragment(RegisterFragment())
+//            }
+//        }, 3000)
+//    }
 }
--- app/src/main/java/dark/composer/carpet/fragments/registrFragments/SplashFragment.kt
+++ app/src/main/java/dark/composer/carpet/fragments/registrFragments/SplashFragment.kt
@@ -6,57 +6,12 @@ import dark.composer.carpet.adapter.CategoryAdapter
 import dark.composer.carpet.adapter.PopularAdapter
 import dark.composer.carpet.databinding.FragmentDefault1Binding
 import dark.composer.carpet.databinding.FragmentDefaultBinding
+import dark.composer.carpet.databinding.FragmentSplashBinding
 import dark.composer.carpet.dto.CategoryModel
 import dark.composer.carpet.fragments.BaseFragment
 
-class SplashFragment : BaseFragment<FragmentDefaultBinding>(FragmentDefaultBinding::inflate) {
-    private val adapterCategory by lazy {
-        CategoryAdapter(requireContext())
-    }
-
-    private val adapterPopular by lazy {
-        PopularAdapter(requireContext())
-    }
-
+class SplashFragment : BaseFragment<FragmentSplashBinding>(FragmentSplashBinding::inflate) {
     override fun onViewCreate() {
-        binding.listCategory.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
-        binding.listPopular.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
-        binding.listCategory.adapter = adapterCategory
-        binding.listPopular.adapter = adapterPopular
 
-        adapterCategory.set(setCategory())
-        adapterPopular.set(setPopular())
-
-        binding.txtCustomers.isSelected = true
-        binding.txtAdmin.isSelected = true
-        binding.txtEmployee.isSelected = true
-        binding.floatingActionButton.setOnClickListener {
-            navController.navigate(R.id.action_splashFragment_to_logInFragment)
-        }
     }
-
-    private fun setCategory():List<CategoryModel>{
-        val list = mutableListOf<CategoryModel>()
-        for (i in 0 until 10){
-            list.add(CategoryModel("Shu zor chiqibti ggggggggggg",R.drawable.carpet,"1203369 cym"))
-        }
-        return list
-    }
-
-    private fun setPopular():List<CategoryModel>{
-        val list = mutableListOf<CategoryModel>()
-        for (i in 0 until 10){
-            list.add(CategoryModel("Shu zor chiqibti",R.drawable.carpet,"1203369 cym"))
-        }
-        return list
-    }
-//    private fun startTimer() {
-//        Handler().postDelayed({
-//            if (shared.getToken() != "") {
-//                extention.controller?.startMainFragment(TrainerFragment())
-//            } else {
-//                extention.controller?.startMainFragment(RegisterFragment())
-//            }
-//        }, 3000)
-//    }
 }
