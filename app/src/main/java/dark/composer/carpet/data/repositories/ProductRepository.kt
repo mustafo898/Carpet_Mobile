7e924546eacc5d98d2ab567171ba2229a86a92d7
mustafo898 <mustafoabdugafurov55@gmail.com>
2022-07-02 16:02:17 +0500

    dsd

--- app/src/main/java/dark/composer/carpet/data/repositories/ProductRepository.kt
+++ app/src/main/java/dark/composer/carpet/data/repositories/ProductRepository.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import dark.composer.carpet.data.retrofit.ApiService
 import dark.composer.carpet.data.retrofit.models.BaseNetworkResult
+import dark.composer.carpet.data.retrofit.models.request.product.ProductCreateRequest
 import dark.composer.carpet.data.retrofit.models.response.factory.PaginationResponse
 import dark.composer.carpet.data.retrofit.models.response.product.ProductResponse
 import javax.inject.Inject
@@ -13,6 +14,23 @@ class ProductRepository @Inject constructor(private var service: ApiService) {
     suspend fun getPagination(page:Int,size:Int,type:String): LiveData<BaseNetworkResult<List<ProductResponse>>> {
         val list = MutableLiveData<BaseNetworkResult<List<ProductResponse>>>()
         val response = service.getProductPagination(page,size,type)
+        list.value = BaseNetworkResult.Loading(true)
+        if (response.code() == 200){
+            response.body()?.let {
+                list.value = BaseNetworkResult.Loading(false)
+                list.value = BaseNetworkResult.Success(it)
+                Log.d("EEEEEEE", "getPagination: $it")
+            }
+        }else{
+            list.value = BaseNetworkResult.Loading(false)
+            list.value = BaseNetworkResult.Error(response.message())
+        }
+        return list
+    }
+
+    suspend fun createProduct(productCreateRequest: ProductCreateRequest): LiveData<BaseNetworkResult<ProductResponse>> {
+        val list = MutableLiveData<BaseNetworkResult<ProductResponse>>()
+        val response = service.createProduct(productCreateRequest)
         list.value = BaseNetworkResult.Loading(true)
         if (response.code() == 200){
             response.body()?.let {
