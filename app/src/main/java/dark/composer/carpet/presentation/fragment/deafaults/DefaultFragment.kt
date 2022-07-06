2b9fe06deb259d0e7b8d51ee2af611644bb3bd6b
mustafo898 <mustafoabdugafurov55@gmail.com>
2022-07-06 18:51:54 +0500

    viewpager add in product fragment

--- app/src/main/java/dark/composer/carpet/presentation/fragment/deafaults/DefaultFragment.kt
+++ app/src/main/java/dark/composer/carpet/presentation/fragment/deafaults/DefaultFragment.kt
@@ -36,6 +36,7 @@ class DefaultFragment : BaseFragment<FragmentDefaultBinding>(FragmentDefaultBind
             providerFactory
         )[DefaultViewModel::class.java]
 
+
         binding.listSale.layoutManager = LinearLayoutManager(
             requireContext(),
             LinearLayoutManager.HORIZONTAL, false
