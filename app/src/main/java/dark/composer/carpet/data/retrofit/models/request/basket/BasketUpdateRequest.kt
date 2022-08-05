a9dbc10f2228c62b76dcd08dd0ee6aaf5552eaa6
SuhrobDev <suhrobqayumov4@gmail.com>
2022-08-05 18:04:06 +0500

    SALE ADDED

--- app/src/main/java/dark/composer/carpet/data/retrofit/models/request/basket/BasketUpdateRequest.kt
+++ app/src/main/java/dark/composer/carpet/data/retrofit/models/request/basket/BasketUpdateRequest.kt
@@ -1,9 +1,9 @@
 package dark.composer.carpet.data.retrofit.models.request.basket
 
-data class BasketUpdateRequest (
+data class BasketUpdateRequest(
     val basketId: Int,
     val status: String,
     val price: Double,
     val amount: Int,
     val height: Double
-    )
+)
---
+++ app/src/main/java/dark/composer/carpet/presentation/fragment/sale/SaleFragment.kt
@@ -0,0 +1,10 @@
+package dark.composer.carpet.presentation.fragment.sale
+
+import dark.composer.carpet.databinding.FragmentSaleBinding
+import dark.composer.carpet.presentation.fragment.BaseFragment
+
+class SaleFragment : BaseFragment<FragmentSaleBinding>(FragmentSaleBinding::inflate) {
+    override fun onViewCreate() {
+
+    }
+}
